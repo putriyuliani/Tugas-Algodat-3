9dd09e4e178488974d87890029e407ac6fd2062e
putriyuliani <pyyuliani@gmail.com>
2017-10-10 21:19:53 +0700

    buat kelas linear list

--- src/tugastiga/LinearList.java
+++ src/tugastiga/LinearList.java
@@ -9,6 +9,14 @@ package tugastiga;
  *
  * @author user
  */
-public class LinearList {
+public interface LinearList {
+   public boolean isEmpty();
+   public int size();
+   public Object get(int index);
+   public int indexOf(Object element);
+   public Object remove();
+   public void add(int index, Object obj);
+   @Override
+   public String toString();
    
 }
