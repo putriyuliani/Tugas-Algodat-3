7cea00c9a43d1edf146d7fc3ebd6fd5d0eb1d4ef
putriyuliani <pyyuliani@gmail.com>
2017-10-10 21:29:15 +0700

    kelas chain node circular

---
+++ src/tugastiga/ChainNodeCircular.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugastiga;
+
+/**
+ *
+ * @author user
+ */
+public class ChainNodeCircular {
+    Object element;
+    ChainNodeCircular next;
+    ChainNodeCircular last;
+    
+    ChainNodeCircular(){
+       
+    }
+    ChainNodeCircular(Object element){
+        this.element=element;
+    }
+    ChainNodeCircular(Object element, ChainNodeCircular next){
+        this.element=element;
+        this.next=next;
+    }
+
+    public  void setLast(ChainNodeCircular last) {
+        this.last = last;
+    }
+}
